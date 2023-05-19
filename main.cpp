bd41d0c14ae9d6dd1be92709273dfd97f8fdd60f
guankai01 <heliar_mk@163.com>
2023-05-19 15:26:11 +0800

    refactor(common.h/common.cc): rename common to utils

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "common.h"
+#include "utils.h"
 #include "triton/core/tritonserver.h"
 #include <chrono>
 #include <cstring>
